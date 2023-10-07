9671ed29f6cc540dddffef3ad1b1695e5910e184
surial <sureshmsfe@gmail.com>
2023-10-07 15:38:01 -0400

    Backend Start

--- fuel/views.py
+++ fuel/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Fuel
+from .serializer import FuelSerializer
+
+
+class FuelViewset(viewsets.ModelViewSet):
+    queryset = Fuel.objects.all()
+    serializer_class = FuelSerializer
 
 # Create your views here.
