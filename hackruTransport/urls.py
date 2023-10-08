652f0b0ce4f4d3a4ecaae5d72851bab9bbde673c
surial <sureshmsfe@gmail.com>
2023-10-07 20:47:02 -0400

    Data

--- hackruTransport/urls.py
+++ hackruTransport/urls.py
@@ -19,11 +19,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from routes.views import RouteViewset
 
+
 router = DefaultRouter()
 router.register('routes', RouteViewset)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+
 ]
